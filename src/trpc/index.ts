7c54791fa92ca0b3b1d58982d9760d8eb25d74d2
aabhiyann <sainjuabhiyan321@gmail.com>
2024-01-02 23:50:26 -0500

    Databse Setup

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,14 +1,36 @@
 import { stringify } from "querystring";
 import { publicProcedure, router } from "./trpc";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
-    const user = getUser();
+    const user = await getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      // create user in the db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
+    return { success: true };
   }),
 });
 
