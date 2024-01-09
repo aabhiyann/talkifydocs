d7b88287b547fae98780046ad571f05ea819f3a0
aabhiyann <sainjuabhiyan321@gmail.com>
2024-01-08 23:15:35 -0500

    Adding delete button

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,13 +2,17 @@
 
 import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
-import { Ghost } from "lucide-react";
+import { Ghost, MessagesSquare, Plus, Trash } from "lucide-react";
 import Skeketon from "react-loading-skeleton";
 import { link } from "fs";
 import Link from "next/link";
+import { format } from "date-fns";
+import { Button } from "./ui/button";
 
 const Dashboard = () => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation();
 
   return (
     <main className="mx-auto max-w-7xl md:p-10">
@@ -37,8 +41,37 @@ const Dashboard = () => {
                 >
                   <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
                     <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-green-500" />
+                    <div className="flex-1 truncate">
+                      <div className="flex items-center space-x-3">
+                        <h3 className="truncate text-lg font-medium text-zinc-900">
+                          {file.name}
+                        </h3>
+                      </div>
+                    </div>
                   </div>
                 </Link>
+
+                <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+                  <div className="flex items-center gap-2">
+                    <Plus className="h-4 w-4" />
+                    {format(new Date(file.createdAt), "MMM yyyy")}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <MessagesSquare className="h-4 w-4" />
+                    Mocked
+                  </div>
+
+                  <Button
+                    onClick={() => {
+                      deleteFile({ id: file.id });
+                    }}
+                    size="sm"
+                    className="w-full"
+                    variant="destructive"
+                  >
+                    <Trash className="h-4 w-4" />
+                  </Button>
+                </div>
               </li>
             ))}
         </ul>
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { stringify } from "querystring";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 // import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { z } from "zod";
 
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
@@ -42,6 +43,28 @@ export const appRouter = router({
       },
     });
   }),
+
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
