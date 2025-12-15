e510a0355213a5386414e13aae6a1b3e00c77826
aabhiyann <sainjuabhiyan321@gmail.com>
2025-12-15 15:59:31 -0500

    chore(db): add migrate script and composite Prisma types

--- src/types/database.ts
+++ src/types/database.ts
@@ -16,4 +16,12 @@ export type DbMessage = Message;
 export type DbHighlight = Highlight;
 export type DbRateLimit = RateLimit;
 
+// Common composite types for richer queries
+export type FileWithUser = File & { user: User };
+export type MessageWithFile = Message & { file: File | null };
+export type ConversationWithRelations = Conversation & {
+  user: User;
+  messages: Message[];
+  conversationFiles: (ConversationFile & { file: File })[];
+};
 
