6f68122a4258b62f8b8decf2de2299524fc8e1d0
aabhiyann <sainjuabhiyan321@gmail.com>
2025-12-17 19:59:49 -0500

    feat: consolidate backend to tRPC and clean up old APIs/actions

--- src/actions/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export server actions
-// Note: "use server" directive is in individual files, not here
-// This allows re-exports to work properly
-
-export { prepareUpload, uploadPDF } from "./upload";
